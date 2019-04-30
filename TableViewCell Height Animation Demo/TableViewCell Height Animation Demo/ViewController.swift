5a8b2471dea50184fc007c65ecdc2b922f9bfdc9
zhouweijie <13522599397@163.com>
2019-04-30 16:24:31 +0800

    将DispatchQueue.main.asyncAfter移动到动画外面，在里面会使得行高变化和动画有一点点不同步

--- TableViewCell Height Animation Demo/TableViewCell Height Animation Demo/ViewController.swift
+++ TableViewCell Height Animation Demo/TableViewCell Height Animation Demo/ViewController.swift
@@ -82,98 +82,93 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         var successF = cell.success.frame
         successF.origin.x = 43
         cell.success.frame = successF
-        UIView.animate(withDuration: animationTime, delay: subcribeImageToCircleDelay, options: [], animations: {
-            
-            var subF = cell.subscribe.frame
-            subF.origin.x = 15
-            subF.origin.y -= 25
-            cell.subscribe.frame = subF
-            
-            var columF = cell.colum.frame
-            columF.origin.y -= 25
-            cell.colum.frame = columF
-            cell.colum.alpha = 0
-            
-            var descriF = cell.descriptionLabel.frame
-            descriF.origin.y -= 25
-            cell.descriptionLabel.frame = descriF
-            cell.descriptionLabel.alpha = 0
-            
-            var avatarF = cell.avatar.frame
-            avatarF.origin.y -= 25
-            cell.avatar.frame = avatarF
-            cell.avatar.alpha = 0
-            
-            var successF = cell.success.frame
-            successF.origin.y -= 25
-            cell.success.frame = successF
-            cell.success.alpha = 1
-
-            var separatorF = cell.separator.frame
-            separatorF.origin.y -= 50
-            cell.separator.frame = separatorF
-            //更新cell高度
-            DispatchQueue.main.asyncAfter(deadline: .now() + self.subcribeImageToCircleDelay, execute: {//使用这行代码，使延时生效
-                UIView.animate(withDuration: self.animationTime, animations: {
-                    let model = self.models[indexPath.row]
-                    model.state = .Panning
-                    self.tableView.beginUpdates()
-                    cell.shouldNotLayout = true
-                    self.tableView.endUpdates()
-                    cell.shouldNotLayout = false
-                })
-            })
-            
-            
-            
-        }, completion: { (complete) in
-            if complete {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + self.subcribeImageToCircleDelay, execute: {//使用这行代码，使beginUpdates+endUpdates延时生效
+            UIView.animate(withDuration: self.animationTime, delay: 0, options: [], animations: {
+                
                 var subF = cell.subscribe.frame
                 subF.origin.x = 15
+                subF.origin.y -= 25
                 cell.subscribe.frame = subF
                 
-                self.moveUpToDisappear(cell: cell, index: indexPath)
-            }
+                var columF = cell.colum.frame
+                columF.origin.y -= 25
+                cell.colum.frame = columF
+                cell.colum.alpha = 0
+                
+                var descriF = cell.descriptionLabel.frame
+                descriF.origin.y -= 25
+                cell.descriptionLabel.frame = descriF
+                cell.descriptionLabel.alpha = 0
+                
+                var avatarF = cell.avatar.frame
+                avatarF.origin.y -= 25
+                cell.avatar.frame = avatarF
+                cell.avatar.alpha = 0
+                
+                var successF = cell.success.frame
+                successF.origin.y -= 25
+                cell.success.frame = successF
+                cell.success.alpha = 1
+                
+                var separatorF = cell.separator.frame
+                separatorF.origin.y -= 50
+                cell.separator.frame = separatorF
+                //更新cell高度
+                let model = self.models[indexPath.row]
+                model.state = .Panning
+                self.tableView.beginUpdates()
+                cell.shouldNotLayout = true
+                self.tableView.endUpdates()
+                cell.shouldNotLayout = false
+                
+            }, completion: { (complete) in
+                if complete {
+                    var subF = cell.subscribe.frame
+                    subF.origin.x = 15
+                    cell.subscribe.frame = subF
+                    
+                    self.moveUpToDisappear(cell: cell, index: indexPath)
+                }
+            })
         })
     }
     
     func moveUpToDisappear(cell: AnimatingCell, index: IndexPath) {
-        UIView.animate(withDuration: animationTime, delay: moveUpDelay, options: [], animations: {
-            var successF = cell.success.frame
-            successF.origin.y -= 50
-            cell.success.frame = successF
-            cell.success.alpha = 0
-            
-            var subF = cell.subscribe.frame
-            subF.origin.y -= 50
-            cell.subscribe.frame = subF
-            cell.subscribe.alpha = 0
-            
-            var separatorF = cell.separator.frame
-            separatorF.origin.y -= 50
-            cell.separator.frame = separatorF
-            //更新cell高度
-            DispatchQueue.main.asyncAfter(deadline: .now() + self.moveUpDelay, execute: {//使用这行代码，使延时生效
-                UIView.animate(withDuration: self.animationTime, animations: {
-                    self.tableView.performBatchUpdates({
-                        let model = self.models[index.row]
-                        model.state = .Dismissing
-                        cell.shouldNotLayout = true
-                    }, completion: { (complete) in
-                        cell.shouldNotLayout = false
-                    })
-                })
-            })
-
-        }, completion: { (complete) in
-            if complete {
+        DispatchQueue.main.asyncAfter(deadline: .now() + self.moveUpDelay, execute: {//使用这行代码，使延时生效
+            UIView.animate(withDuration: self.animationTime, delay: 0, options: [], animations: {
+                var successF = cell.success.frame
+                successF.origin.y -= 50
+                cell.success.frame = successF
+                cell.success.alpha = 0
+                
+                var subF = cell.subscribe.frame
+                subF.origin.y -= 50
+                cell.subscribe.frame = subF
+                cell.subscribe.alpha = 0
+                
+                var separatorF = cell.separator.frame
+                separatorF.origin.y -= 50
+                cell.separator.frame = separatorF
+                //更新cell高度
                 self.tableView.performBatchUpdates({
-                    self.models.remove(at: index.row)
-                    self.tableView.deleteRows(at: [index], with: UITableView.RowAnimation.none)
+                    let model = self.models[index.row]
+                    model.state = .Dismissing
+                    cell.shouldNotLayout = true
                 }, completion: { (complete) in
-                    
+                    cell.shouldNotLayout = false
                 })
-            }
+                
+            }, completion: { (complete) in
+                if complete {
+                    self.tableView.performBatchUpdates({
+                        self.models.remove(at: index.row)
+                        self.tableView.deleteRows(at: [index], with: UITableView.RowAnimation.none)
+                    }, completion: { (complete) in
+                        
+                    })
+                }
+            })
         })
     }
 }
